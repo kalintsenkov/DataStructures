2f1cffb2ca8d414ab0e84040c0d600b2e3739644
Kalin Tsenkov <Kalin.Tsenkov@indeavr.com>
2021-08-04 17:02:19 +0300

    Implemented MaxHeap

--- src/MaxHeap/MaxHeap.Tests.cs
+++ src/MaxHeap/MaxHeap.Tests.cs
@@ -1,43 +1,61 @@
 namespace MaxHeap
 {
+    using System;
     using System.Collections.Generic;
     using NUnit.Framework;
 
     public class MaxHeapTests
     {
-        //private MaxHeap<int> integerHeap;
+        private MaxHeap<int> integerHeap;
 
-        //[SetUp]
-        //public void InitializeHeap()
-        //    => this.integerHeap = new MaxHeap<int>();
+        [SetUp]
+        public void InitializeHeap()
+            => this.integerHeap = new MaxHeap<int>();
 
-        //[Test]
-        //public void TestHeapifyUpAddOne()
-        //{
-        //    this.integerHeap.Add(13);
-        //    Assert.AreEqual(13, this.integerHeap.Peek());
-        //}
+        [Test]
+        public void TestPeekShouldBeCorrect()
+        {
+            var elements = new List<int> { 15, 25, 6, 9, 5, 8, 17, 16 };
+            foreach (var element in elements)
+            {
+                this.integerHeap.Add(element);
+            }
+            Assert.AreEqual(25, this.integerHeap.Peek());
+        }
 
-        //[Test]
-        //public void TestHeapifyUpAddMany()
-        //{
-        //    var elements = new List<int> { 15, 6, 9, 5, 25, 8, 17, 16 };
-        //    foreach (var element in elements)
-        //    {
-        //        this.integerHeap.Add(element);
-        //    }
-        //    Assert.AreEqual(25, this.integerHeap.Peek());
-        //}
+        [Test]
+        public void TestPeekShouldThrowExceptionIfHeapIsEmpty()
+        {
+            Assert.Throws<IndexOutOfRangeException>(() => this.integerHeap.Peek());
+        }
 
-        //[Test]
-        //public void TestSizeShouldBeCorrect()
-        //{
-        //    var elements = new List<int> { 15, 25, 6, 9, 5, 8, 17, 16 };
-        //    foreach (var element in elements)
-        //    {
-        //        this.integerHeap.Add(element);
-        //    }
-        //    Assert.AreEqual(8, this.integerHeap.Size);
-        //}
+        [Test]
+        public void TestHeapifyUpAddOne()
+        {
+            this.integerHeap.Add(13);
+            Assert.AreEqual(13, this.integerHeap.Peek());
+        }
+
+        [Test]
+        public void TestHeapifyUpAddMany()
+        {
+            var elements = new List<int> { 15, 6, 9, 5, 25, 8, 17, 16 };
+            foreach (var element in elements)
+            {
+                this.integerHeap.Add(element);
+            }
+            Assert.AreEqual(25, this.integerHeap.Peek());
+        }
+
+        [Test]
+        public void TestSizeShouldBeCorrect()
+        {
+            var elements = new List<int> { 15, 25, 6, 9, 5, 8, 17, 16 };
+            foreach (var element in elements)
+            {
+                this.integerHeap.Add(element);
+            }
+            Assert.AreEqual(8, this.integerHeap.Size);
+        }
     }
 }
--- src/MaxHeap/MaxHeap.cs
+++ src/MaxHeap/MaxHeap.cs
@@ -1,19 +1,82 @@
 ﻿namespace MaxHeap
 {
     using System;
+    using System.Collections.Generic;
 
     public class MaxHeap<T> where T : IComparable<T>
     {
-        public int Size => throw new NotImplementedException();
+        private readonly List<T> heap = new();
 
-        public void Add(T element)
-        {
-            throw new NotImplementedException();
-        }
+        public int Size => this.heap.Count;
 
         public T Peek()
         {
+            this.CheckSize();
+
+            return this.heap[0];
+        }
+
+        public void Add(T value)
+        {
+            this.heap.Add(value);
+            this.HeapifyUp(this.Size - 1);
+        }
+
+        public T Extract()
+        {
+            this.CheckSize();
+
+            var max = this.heap[0];
+            var last = this.heap[this.Size - 1];
+
+            this.heap[0] = last;
+
+            this.heap.Remove(last);
+
+            this.HeapifyDown(this.Size - 1);
+
+            return max;
+        }
+
+        private void HeapifyUp(int index)
+        {
+            if (index == 0)
+            {
+                return;
+            }
+
+            var parentIndex = this.GetParentIndex(index);
+
+            if (this.heap[index].CompareTo(this.heap[parentIndex]) <= 0)
+            {
+                return;
+            }
+
+            this.Swap(index, parentIndex);
+
+            this.HeapifyUp(parentIndex);
+        }
+
+        private void HeapifyDown(int index)
+        {
             throw new NotImplementedException();
         }
+
+        private void CheckSize()
+        {
+            if (this.Size == 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        private void Swap(int index, int parentIndex)
+            => (this.heap[index], this.heap[parentIndex]) = (this.heap[parentIndex], this.heap[index]);
+
+        private int GetParentIndex(int index) => (index - 1) / 2;
+
+        private int GetLeftChildIndex(int index) => 2 * index + 1;
+
+        private int GetRightChildIndex(int index) => 2 * index + 2;
     }
 }
